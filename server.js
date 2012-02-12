ba4e2f957d1004811f108698be466a2c14c008a9
Joachim Kainz <jfk@jolira.com>
2012-02-11 23:46:09 -0800

    adding default configuration files for demos.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var config = require("./lib/config");
 var server = require("./lib/server");
+var path = require("path");
 
 var home = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 var candidates = [
+        path.join(".", ".subversion-hub.json"),
         path.join(home, ".subversion-hub.json")
 ];
 
